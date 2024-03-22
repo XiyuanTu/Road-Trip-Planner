6db2855038419b2c7f7ccb038ba573b57b025689
junjiefang <fang.junj@northeastern.edu>
2024-03-21 18:17:34 -0700

    delete a package and update token expiration

--- server/API/users.js
+++ server/API/users.js
@@ -24,7 +24,7 @@ router.post('/login', async (req, res) => {
     const token = jwt.sign(
       { id: user._id },
       process.env.JWT_SECRET_KEY,
-      { expiresIn: '2400h' },
+      { expiresIn: '30m' },
     );
 
     res.json({ token });
